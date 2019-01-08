4bfcd1c569c643db5336a46ac7e0fa9e81e6354e
yousseftange17 <46229103+yousseftange17@users.noreply.github.com>
2019-01-08 15:51:56 +0000

    Update test.js

--- test.js
+++ test.js
@@ -4,45 +4,33 @@ const client = new Discord.Client();
 
 const devs = ["497482507273502720" , "497482507273502720"]
 
-const adminprefix = "*";
+const adminprefix = "البرفكس ";
+const developers = ['ايدي حقك'];
+
+
+client.on('ready', () => {
+    console.log(`Logged as ${client.user.tag}`)
+})
+
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!devs.includes(message.author.id)) return;
-      
+      if (!developers.includes(message.author.id)) return;
   if (message.content.startsWith(adminprefix + 'ply')) {
     client.user.setGame(argresult);
-      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
-  } else
+      message.channel.send(``)
+  } else 
   if (message.content.startsWith(adminprefix + 'wt')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+      message.channel.send(``)
   } else 
   if (message.content.startsWith(adminprefix + 'ls')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
-  } else    
+      message.channel.send(``)
+  } else
   if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/osama_gmt");
-      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
-  }});
-
-client.on('ready', function(){
-    client.user.setStatus("dnd");
-    var ms = 10000 ;
-    var setGame = [`Youssef The Best 𖡃`];
-    var i = -1;
-    var j = 0;
-    setInterval(function (){
-        if( i == -1 ){
-            j = 1;
-        }
-        if( i == (setGame.length)-1 ){
-            j = -1;
-        }
-        i = i+j;
-        client.user.setGame(setGame[i],`https://www.twitch.tv/osama_gmt`);
-    }, ms);10000
-
+    client.user.setGame(argresult, "https://www.twitch.tv/Randy");
+      message.channel.send(``)
+  }
 });
 
 
