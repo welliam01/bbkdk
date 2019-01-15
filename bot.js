63825b1e0fe692e4faa11c94fb8818e3386e8348
yousseftange17 <46229103+yousseftange17@users.noreply.github.com>
2019-01-15 16:03:55 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const adminprefix = "o";
-const developers = ["440081527909515265" , "474200581163057153"];
+const adminprefix = "*";
+const developers = ["497482507273502720" , "474200581163057153"];
 
 
 client.on('ready', () => {
@@ -27,19 +27,6 @@ client.on('message', message => {
   if (message.content.startsWith(adminprefix + 'st')) {
     client.user.setGame(argresult, "https://www.twitch.tv/osama_gmt");
       message.channel.send(``)
-  }
-});
-
-
-client.on('message', msg => {
-  if (msg.content === 'مليكانو') {
-    msg.channel.send(`\`\`\`عيون مليكانو إذا ما رديت كلمني إنستا jor.io\`\`\``);
-  }
-});
-
-client.on('message', msg => {
-  if (msg.content === '<@440081527909515265>') {
-    msg.channel.send(`\`\`\`عيون مليكانو إذا ما رديت كلمني إنستا jor.io\`\`\``);
   }
 });
 
